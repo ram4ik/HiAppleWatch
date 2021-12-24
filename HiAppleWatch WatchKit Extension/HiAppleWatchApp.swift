c94dca136519f57dc542fe585cfca5c12806a683
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-24 12:44:26 +0200

    Initial Commit

---
+++ HiAppleWatch WatchKit Extension/HiAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HiAppleWatchApp.swift
+//  HiAppleWatch WatchKit Extension
+//
+//  Created by ramil on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HiAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
